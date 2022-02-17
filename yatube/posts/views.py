16b4c9fefbe71206fde3af7a7fcd72d14e431c6b
Артем Громов <freemirror@ya.ru>
2022-02-18 02:14:33 +0300

    creating templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Добро пожаловать на YAtube')
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_list(request):
-    return HttpResponse('Список сообществ')
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 def group_posts(request, slug):
     return HttpResponse(f'Сообщества {slug}')
